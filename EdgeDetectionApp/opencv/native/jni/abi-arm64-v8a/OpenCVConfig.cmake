453743f96a983c9bdacc5752d5ad48ef06f86150
Vishnu R <vishnuradhakrishna45@gmail.com>
2025-11-14 02:09:34 +0530

    moved the native/jnl folder t opencv

--- EdgeDetectionApp/opencv/native/jni/abi-arm64-v8a/OpenCVConfig.cmake
+++ EdgeDetectionApp/opencv/native/jni/abi-arm64-v8a/OpenCVConfig.cmake
@@ -1,5 +1,5 @@
 # Path to the folder that contains OpenCVConfig.cmake
-set(OpenCV_DIR ${CMAKE_SOURCE_DIR}V:/projects/flamapp/OpenCV-android-sdk/sdk/native/jni/abi-arm64-v8a)
+set(OpenCV_DIR ${CMAKE_SOURCE_DIR}/../../opencv/native/jni)
 
 find_package(OpenCV REQUIRED)
 
